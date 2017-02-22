ebd2e3aed348bd20ad20a87d2111e36bd9c6aa4c
Maxim Salnikov <salnikov@gmail.com>
2017-02-22 11:30:27 +0100

    opbeat added

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://mbroker.forgerocklabs.net/')
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
+var mqtt = require('mqtt')
+var mqttClient = mqtt.connect('mqtt://mbroker.forgerocklabs.net/')
+
+var W3CWebSocket = require('websocket').w3cwebsocket;
+var wsClient = new W3CWebSocket('ws://mbroker.forgerocklabs.net/', 'echo-protocol');
+
 var io = require('socket.io')(server);
 
 var base64 = require('base-64');
-var http = require('http');
 
 var cors = require('cors');
 app.use(cors());
 app.options('*', cors());
+
 var bodyParser = require('body-parser');
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 const webPush = require('web-push');
-let pushSubscription;
+var pushSubscription;
+
+var opbeat = require('opbeat').start({
+  appId: '907f105189',
+  organizationId: 'a4e28844078e4e7f8b7b0ea31d243593',
+  secretToken: '0ae0e3d54951fe60c3319d63c7d8982902894fc0'
+})
+
+app.use(opbeat.middleware.express())
 
 // The GCM API key is AIzaSyDNlm9R_w_0FDGjSM1fzyx5I5JnJBXACqU
 webPush.setVapidDetails(
@@ -24,22 +38,20 @@ webPush.setVapidDetails(
     's-zBxZ1Kl_Y1Ac8_uBjwIjtLtG6qlJKOX5trtbanAhc'
 );
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-
 app.post("/subscription", function(req, res, next) {
 
     if (req.body.action === 'subscribe') {
         pushSubscription = req.body.subscription;
 
-        console.log('subscription called with ' + pushSubscription)
+        console.log('Subscription registered: ')
+        console.log(JSON.stringify(pushSubscription, null, 2))
 
     } else {
         throw new Error('Unsupported action');
     }
 
     res.send({
-        text: 'Sending push in 5',
+        text: 'Subscribed',
         status: "200"
     });
 });
@@ -47,34 +59,41 @@ app.post("/subscription", function(req, res, next) {
 function sendNotification(payload) {
 
     if (pushSubscription) {
-        console.log('sendNotification with ', payload)
 
         webPush.sendNotification(pushSubscription, payload)
             .then(function(response) {
-                if (response) {
-                    console.log("PUSH RESPONSE: ", response);
-                } else {
-                    console.log("PUSH SENT");
-                }
+                console.log("Push sent");
+                //console.log(JSON.stringify(response, null, 2))
             })
             .catch(function(err) {
-                console.error("PUSH ERR: " + err);
+                console.error("Push error: " + err);
             });
-    } else {
-      console.log('No push subscription found')
+
+    }
+}
+
+function sendToBrowser(messageContent) {
+
+    messageContent.notification = {
+        title: messageContent.topic,
+        body: messageContent.message
     }
 
+    io.emit('deviceMessage', messageContent);
+    sendNotification(JSON.stringify(messageContent))
 
 }
 
-client.on('connect', function() {
-    console.log('Broker connected');
-    client.subscribe('#')
+// MQTT Client
+
+mqttClient.on('connect', function() {
+    console.log('MQTT Client connected');
+    mqttClient.subscribe('#')
 })
 
-client.on('message', function(topic, message) {
+mqttClient.on('message', function(topic, message) {
 
-    console.log('Message from broker received')
+    console.log('Message from MQTT Client received')
     console.log('Topic:' + topic.toString())
     console.log('Message:' + message.toString())
 
@@ -83,39 +102,69 @@ client.on('message', function(topic, message) {
         message: message.toString()
     }
 
-    // Sending to client
-    io.emit('deviceMessage', messageContent);
+    sendToBrowser(messageContent);
 
-    sendNotification(JSON.stringify(messageContent))
 })
+
+
+
+// Browser connection
 
 io.on('connection', function(socket) {
 
-    console.log('Client connected');
+    console.log('Browser connected');
 
     socket.on('disconnect', function() {
-        console.log('Client disconnected');
+        console.log('Browser disconnected');
     });
 
     socket.on('deviceMessage', (messageContent) => {
 
-        console.log('Message from client received')
+        console.log('Message from browser received')
         console.log('Topic:' + messageContent.topic)
         console.log('Message:' + messageContent.message)
 
-        // Sending to client
-        io.emit('deviceMessage', {
-            topic: messageContent.topic,
-            message: messageContent.message
-        });
+        // Sending to browser
+        io.emit('deviceMessage', messageContent);
 
-        // Sending to broker
-        client.publish(messageContent.topic, messageContent.message)
+        // Sending to MQTT client
+        mqttClient.publish(messageContent.topic, messageContent.message)
+
+        // Sending to WS client
+        if (wsClient.readyState === wsClient.OPEN) {
+            wsClient.send(JSON.stringify(messageContent));
+        }
 
     });
 
 });
 
+// WS client
+
+wsClient.onerror = function() {
+    console.log('WebSocket Client Connection Error');
+};
+
+wsClient.onopen = function() {
+    console.log('WebSocket Client Connected');
+};
+
+wsClient.onclose = function() {
+    console.log('WebSocket Client Closed');
+};
+
+wsClient.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+
+        console.log("Message from MQTT Client: '" + e.data + "'");
+        sendToBrowser(e.data);
+
+    }
+};
+
+
+// Starting server
+
 server.listen(process.env.PORT || 3001, function() {
-    console.log('Listening on port ' + process.env.PORT || 3001)
+    console.log('Listening on port ' + ( process.env.PORT || 3001 ))
 })
